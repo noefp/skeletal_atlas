120f07473484c75eefcf00f4fce9811af32b5d11
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-04-03 17:07:40 +0200

    basic atlses description improved

--- egdb_custom_text/expr_datasets/00_human_tissues_atlas.php
+++ egdb_custom_text/expr_datasets/00_human_tissues_atlas.php
@@ -1,7 +1,6 @@
-<p>
-  RNA-seq of muskuloskeletal tissues from <i>Homo sapiens</i> 
-</p>
-<h4>Tissues</h4>
+
+<h2>RNA-seq of muskuloskeletal tissues from <i>Homo sapiens</i></h2> 
+<h3>Tissues</h3>
   <ul>
     <li>Articular cartilage</li>
     <li>Epyphisis</li>
@@ -10,121 +9,124 @@
     <li>Tendon</li>
     <li>Muscle</li>
   </ul>
-
+  <h3>Articular cartilage</h3>
   <p>
-    Articular cartilage
-    
-    Datasets come from two works:
-
-    1) This dataset was published by <a href=" https://doi.org/10.1002/term.2961" target="_blank">Vail et al., 2020</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA528088" target="_blank">PRJNA528088</a>.
-    Tissue source: Articular cartilage from the femoral condyle and/or tibial plateau. (Articular Engineering, LLC)
-    Age: 20/20/35 years old
-    Sex: Both
-    Layout: PAIRED
-    Experimental procedure: Tissue was snapped frozen in liquid nitrogen and mechanical crushed followed by RNA extraction with trizol. 
+    Datasets come from two works:<br>
+    <br>
+    1) This dataset was published by <a href=" https://doi.org/10.1002/term.2961" target="_blank">Vail et al., 2020</a>
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA528088" target="_blank">PRJNA528088</a>.<br>
+    Tissue source: Articular cartilage from the femoral condyle and/or tibial plateau. (Articular Engineering, LLC).<br>
+    Age: 20/20/35 years old.<br>
+    Sex: Both.<br>
+    Layout: PAIRED.<br>
+    Reads length: 150 bp.<br>
+    Sample processing: Tissue was snapped frozen in liquid nitrogen and mechanical crushed followed by RNA extraction with trizol. 
     All samples underwent trizol phase separation of nucleic acids followed by QIAGEN column RNA purification (catalog number 74194)
     Strand specific cDNA (250-300bp) libraries constructed using the NEBNext Ultra Kit for low input (5ng-1000ng) following poly-T oligo-attached magnetic bead purification. Quality assessed by insert size (Agilent 2100).
-    Sequencing was performed with Illumina HiSeq 4000 and normalized to TPM.
-
+    Sequencing was performed with Illumina HiSeq 4000 and normalized to TPM.<br>
+    <br>
     2) This dataset was published by <a href="https://doi.org/10.1016/j.joca.2018.07.012" target="_blank">Fisch et al., 2018</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA454873" target="_blank">PRJNA454873</a>.
-    Tissue source: Knee articular cartilage from different donors
-    Age: Adult
-    Sex: Both
-    Layout: SINGLE
-    Experimental procedure: Cartilage was stored at -20ºC in Allprotect Tissue Reagent immediately after harvest until RNA extraction. For RNA isolation, tissue was pulverized and homogenized in Qiazol Lysis Reagent. To remove proteins and cellular debris, a initial phenol-chloroform extraction was performed.
-    mRNA libraries were prepared using the Encore Complete RNA-Seq DR Multiplex System 1-8 and 9-16 (NuGen) with 16 unique indexed adapters.
-    Sequencing was performed with Illumina HiSeq 2000 and normalized to TPM.
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA454873" target="_blank">PRJNA454873</a>.<br>
+    Tissue source: Knee articular cartilage from different donors.<br>
+    Age: 18-61 years old, mean 38.<br>
+    Sex: Both.<br>
+    Layout: SINGLE.<br>
+    Reads length: 75 bp.<br>
+    Sample processing: Cartilage was stored at -20ºC in Allprotect Tissue Reagent immediately after harvest until RNA extraction. For RNA isolation, tissue was pulverized and homogenized in Qiazol Lysis Reagent. To remove proteins and cellular debris, a initial phenol-chloroform extraction was performed.
+    mRNA libraries were prepared using the Encore Complete RNA-Seq DR Multiplex System 1-8 and 9-16 (NuGen) with 16 unique indexed adapters. Sequencing was performed with Illumina HiSeq 2000 and normalized to TPM.<br>
 
 </p>
+<h3>Growth plate</h3>
 <p>
-    Epiphysis
-    
+    <b>Epiphysis</b><br>
+    <br>
     This dataset was published by <a href="https://doi.org/10.7554/eLife.79925" target="_blank">Richard et al., 2023</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA801843" target="_blank">PRJNA801843</a>.
-    Tissue source: Epiphyseal chondrocytes
-    Age: Fetal, late first-trimester and early second-trimester terminations
-    Sex: Not specified
-    Layout: PAIRED
-    Experimental procedure: Microdissected fetal cartilage was minced and subsequently incubated with collagenase.  Liberated cells were pelleted by centrifugation and lysed in guanidine thiocyanate buffer. Total RNA was purified using silica column-based kits. 
-    Libraries were constructed using the TruSeq RNA Library Prep Kit v2 (Illumina, San Diego). Sequencing was performed with Illumina NextSeq 500 and normalized to TPM.
-
-</p>
-<p>
-    Growth plate
-    
-    Datasets come from two works:
-
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA801843" target="_blank">PRJNA801843</a>.<br>
+    Tissue source: Epiphyseal chondrocytes.<br>
+    Age: Fetal, late first-trimester and early second-trimester terminations.<br>
+    Sex: Not specified.<br>
+    Layout: PAIRED.<br>
+    Reads length: 150 bp.<br>
+    Sample processing: Microdissected fetal cartilage was minced and subsequently incubated with collagenase.  Liberated cells were pelleted by centrifugation and lysed in guanidine thiocyanate buffer. Total RNA was purified using silica column-based kits. 
+    Libraries were constructed using the TruSeq RNA Library Prep Kit v2 (Illumina, San Diego). Sequencing was performed with Illumina NextSeq 500 and normalized to TPM.<br>
+    <br>
+    <b>Growth plate</b><br>
+    <br>
+    Datasets come from two works:<br>
+    <br>
     1) This dataset was published by <a href="https://doi.org/10.7554/eLife.79925" target="_blank">Richard et al., 2023</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA801843" target="_blank">PRJNA801843</a>.
-    Tissue source: Growth plate chondrocytes
-    Age: Fetal, late first-trimester and early second-trimester terminations
-    Sex: Not specified
-    Layout: PAIRED
-    Experimental procedure: Microdissected fetal cartilage was minced and subsequently incubated with collagenase.  Liberated cells were pelleted by centrifugation and lysed in guanidine thiocyanate buffer. Total RNA was purified using silica column-based kits. 
-    Libraries were constructed using the TruSeq RNA Library Prep Kit v2 (Illumina, San Diego). Sequencing was performed with Illumina NextSeq 500 and normalized to TPM.
-
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA801843" target="_blank">PRJNA801843</a>.<br>
+    Tissue source: Growth plate chondrocytes.<br>
+    Age: Fetal, late first-trimester and early second-trimester terminations.<br>
+    Sex: Not specified.<br>
+    Layout: PAIRED.<br>
+    Reads length: 150 bp.<br>
+    Sample processing: Microdissected fetal cartilage was minced and subsequently incubated with collagenase.  Liberated cells were pelleted by centrifugation and lysed in guanidine thiocyanate buffer. Total RNA was purified using silica column-based kits. 
+    Libraries were constructed using the TruSeq RNA Library Prep Kit v2 (Illumina, San Diego). Sequencing was performed with Illumina NextSeq 500 and normalized to TPM.<br>
+    <br>
     2) This dataset was published by <a href="https://doi.org/10.1186/s12864-017-4378-y" target="_blank">Li et al., 2017</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA420979" target="_blank">PRJNA420979</a>.
-    Tissue source: Growth plate chondrocytes
-    Age: Fetal (14 to 18 weeks old)
-    Sex: Not specified
-    Layout: PAIRED
-    Experimental procedure: RNA was isolated and purified from five independent 14-18 week distal human femur growth plate cartilage samples. The fetal cartilage was digested in collagenase, the chondrocytes were collected by centrifugation and total RNA was isolated with TRIZol (Thermo Fisher Scientific). Genomic DNA contamination was removed by DNase I (Thermo Fisher Scientific) digestion and the RNA was further purified with the RNeasy Mini Kit (Qiagen).
-    PolyA-tailed mRNA was isolated from total RNA and used as the input for sequencing library construction with the TruSeq RNA Preparation Kit (Illumina). Sequencing was performed with Illumina Hiseq 2000 and normalized to TPM.
-
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA420979" target="_blank">PRJNA420979</a>.<br>
+    Tissue source: Growth plate chondrocytes.<br>
+    Age: Fetal (14 to 18 weeks old).<br>
+    Sex: Not specified.<br>
+    Layout: PAIRED.<br>
+    Reads length: 75 bp.<br>
+    Sample processing: RNA was isolated and purified from five independent 14-18 week distal human femur growth plate cartilage samples. The fetal cartilage was digested in collagenase, the chondrocytes were collected by centrifugation and total RNA was isolated with TRIZol (Thermo Fisher Scientific). Genomic DNA contamination was removed by DNase I (Thermo Fisher Scientific) digestion and the RNA was further purified with the RNeasy Mini Kit (Qiagen).
+    PolyA-tailed mRNA was isolated from total RNA and used as the input for sequencing library construction with the TruSeq RNA Preparation Kit (Illumina). Sequencing was performed with Illumina Hiseq 2000 and normalized to TPM.<br>
 
 </p>
+<h3>Bone</h3>
 <p>
-    Bone
-
-    Datasets come from two works:
     
+    Datasets come from two works:<br>
+    <br>
     1) This dataset was published by <a href="https://doi.org/10.1371/journal.pone.0138347" target="_blank">Farr et al., 2015</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA295101" target="_blank">PRJNA295101</a>.
-    Tissue source: Iliac crest bone
-    Age: Young women 
-    Sex: Women
-    Layout: PAIRED
-    Experimental procedure: RNA-seq profiles were obtained from small needle bone biopsies from the posterior iliac crest of all subjects using an 8G needle under local anesthesia
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA295101" target="_blank">PRJNA295101</a>.<br>
+    Tissue source: Iliac crest bone.<br>
+    Age: 22-40 years old, mean 30.<br>
+    Sex: Women.<br>
+    Layout: PAIRED.<br>
+    Reads length: 50 bp.<br>
+    Sample processing: RNA-seq profiles were obtained from small needle bone biopsies from the posterior iliac crest of all subjects using an 8G needle under local anesthesia
     total RNA was isolated using the RNeasy Micro Kit (Qiagen, Valencia, CA) and treated with the Turbo DNA-freeTM Kit (Life Technologies, Grand Island, NY) to remove potential contaminating DNA that may lead to false-positive amplification.
-    Sequencing was performed with Illumina HiSeq 2000 and normalized to TPM.
-
+    Sequencing was performed with Illumina HiSeq 2000 and normalized to TPM.<br>
+    <br>
     2) This dataset was published by <a href="https://doi.org/10.1172/JCI159928" target="_blank">Martinez-Calle et al., 2023</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA798962" target="_blank">PRJNA798962</a>.
-    Tissue source: Iliac crest bone
-    Age: Mid age
-    Sex: Women
-    Layout: SINGLE
-    Experimental procedure:  From iliac crest bone biopsies, total RNA was isolated using TRI reagent (Waltham, MA, USA) and purified RNA using RNeasy kit (Qiagen, Germantown, MD, USA) and submitted 1 µg of total RNA for sequencing.
-    Libraries were prepared according to Illumina's instructions, i.e, total RNA library for each individual sample was prepared using the TruSeq Total RNA-Seq Library Preparation Kit (Illumina, San Diego, CA), and the bar-coded cDNA libraries were sequenced for 100 bp single reads using Illumina HiSeq 4000.
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA798962" target="_blank">PRJNA798962</a>.<br>
+    Tissue source: Iliac crest bone.<br>
+    Age: Mean 40 years old.<br>
+    Sex: Women.<br>
+    Layout: SINGLE.<br>
+    Reads length: 100 bp.<br>
+    Sample processing: From iliac crest bone biopsies, total RNA was isolated using TRI reagent (Waltham, MA, USA) and purified RNA using RNeasy kit (Qiagen, Germantown, MD, USA) and submitted 1 µg of total RNA for sequencing.
+    Libraries were prepared according to Illumina's instructions, i.e, total RNA library for each individual sample was prepared using the TruSeq Total RNA-Seq Library Preparation Kit (Illumina, San Diego, CA), and the bar-coded cDNA libraries were sequenced for 100 bp single reads using Illumina HiSeq 4000.<br>
+
 </p>
+<h3>Tendon</h3>
 <p>
-    Tendon
-    
+
     This dataset was published by <a href="https://doi.org/10.1016/j.jare.2018.08.001" target="_blank">Zheng et al., 2018</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA750927" target="_blank">PRJNA429184</a>.
-    Tissue source: Tendon
-    Age: Adult  
-    Sex: Both
-    Layout: PAIRED
-    Experimental procedure: Tendon tissues were removed, flash frozen on dry ice, and RNA was harvested using Trizol reagent. Illumina TruSeq Standard Total RNA Prep Kit was used for the construction of sequencing libraries.
-    RNA libraries were prepared for sequencing using standard Illumina protocols. Sequencing was performed with Illumina HiSeq 4000 and normalized to TPM.
-
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA429184" target="_blank">PRJNA429184</a>.<br>
+    Tissue source: Tendon.<br>
+    Age: Adult (not specified).<br>
+    Sex: Both.<br>
+    Layout: PAIRED.<br>
+    Reads length: 150 bp.<br>
+    Sample processing: Tendon tissues were removed, flash frozen on dry ice, and RNA was harvested using Trizol reagent. Illumina TruSeq Standard Total RNA Prep Kit was used for the construction of sequencing libraries.
+    RNA libraries were prepared for sequencing using standard Illumina protocols. Sequencing was performed with Illumina HiSeq 4000 and normalized to TPM.<br>
 
 </p>
+<h3>Muscle</h3>
 <p>
-    Muscle
-    
-    This dataset was published by <a href="https://doi.org/10.3390/cells11091508" target="_blank">Depuydt et al., 2022</a> 
-    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA837196" target="_blank">PRJNA837196</a>.
-    Tissue source: Vastus lateralis, Semimembranosus and Rectus Femoris muscles
-    Age: Adult
-    Sex: Men
-    Layout: PAIRED
-    Experimental procedure: RNA was extracted from muscle biopsies using the Direct-zol RNA Miniprep Plus kit ZY-R2073 (Zymo Research, Irvine, CA, USA). Samples were treated with DNase prior to their submission to the VIB Nucleomics Core (www.nucleomics.be). RNA concentration and purity were determined spectrophotometrically using the Nanodrop ND-1000 (Nanodrop Technologies, Wilmington, DE, USA). Libraries were constructed using the SMART-Seq Stranded Kit (Takara Bio USA, San Jose, CA, USA).
-    Sequencing was performed with Illumina NovaSeq 6000 and normalized to TPM.
 
+    This dataset was published by <a href="https://doi.org/10.3390/cells11091508" target="_blank">Depuydt et al., 2022</a> 
+    and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA837196" target="_blank">PRJNA837196</a>.<br>
+    Tissue source: Vastus lateralis, Semimembranosus and Rectus Femoris muscles.<br>
+    Age: 26-64 years old, mean 45,7.<br>
+    Sex: Men.<br>
+    Layout: PAIRED.<br>
+    Reads length: 50 bp.<br>
+    Sample processing: RNA was extracted from muscle biopsies using the Direct-zol RNA Miniprep Plus kit ZY-R2073 (Zymo Research, Irvine, CA, USA). Samples were treated with DNase prior to their submission to the VIB Nucleomics Core (www.nucleomics.be). RNA concentration and purity were determined spectrophotometrically using the Nanodrop ND-1000 (Nanodrop Technologies, Wilmington, DE, USA). Libraries were constructed using the SMART-Seq Stranded Kit (Takara Bio USA, San Jose, CA, USA).
+    Sequencing was performed with Illumina NovaSeq 6000 and normalized to TPM.<br>
 
 </p>
