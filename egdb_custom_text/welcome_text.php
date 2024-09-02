da4c5c3e42162c0d41e52bdfe71e9edf7f3729d8
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-09-02 17:40:16 +0200

    new home page style and navigation

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -1,101 +1,70 @@
-
-  <div class="row" style='margin-left:0px; margin-right:0px; ?>'>
-      
-      <div class="container-fluid" style="padding:0px;">
-        
-        <div class="cover_img">
-          <h1 id="welcome_h1"><img src='<?php echo "$images_path/db_logo_large.png";?>' alt="DB_Logo" style="height:200px;"> Welcome to the Skeletal Atlas</h1>
-          <div id="button_div">
-            <a id="start_button" href="/easy_gdb/tools/expression/expression_input.php" class="btn btn-info">Start</a>
-          </div>
-        </div>
-      
-     </div>
-
+<!-- In this file the welocome page is created where you can select the worck tools -->
+<html>
+  <div class="cover_img">
+    <h1 id="welcome_h1"><img src='<?php echo "$images_path/db_logo_large.png";?>' alt="Mouse_Logo" style="height:200px;"> Welcome to the Skeletal Atlas</h1>
   </div>
 
-
-<!-- Construction Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Warning!</h4>
+  <div id="box">
+    <div class="row">
+      <div class="select">
+        <a href="/easy_gdb/tools/expression/comparator_input.php" class="d-flex zoom-img">
+          <img src="<?php echo $images_path.'/tools/exp_comparator.png'?>" alt="expression viewer" width="250px" height="250px" class="solid alignnone size-medium wp-image-3011 rounded-circle image">
+        </a>
+        <div class="text">
+          <strong>Expression Comparator</strong>
+        </div>
       </div>
-
-      <!-- Modal body -->
-      <div class="modal-body" style="text-align: center;">
-         <img src='<?php echo "$images_path/warning.png";?>' />
-<br>
-<br>
-        This site is under construction!
+      <div class="select">
+        <a href="/skeletal_atlas/egdb_custom_text/tools/expression/expression_input.php" class="d-flex zoom-img">
+          <img src="<?php echo $images_path.'/tools/multi_reference.png'?>" alt="reference" width="350px" height="350px" class="solid alignnone size-medium wp-image-3011 rounded-circle image" >
+        </a>
+        <div class="text">
+          <strong>Multiomic Reference Atlas</strong>
+        </div>
       </div>
-
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      <div class="select">
+        <a href="/easy_gdb/tools/expression/expression_input.php" class="d-flex zoom-img">
+          <img src="<?php echo $images_path.'/tools/exp_expre_viewer.png'?>" alt="Comparator" width="250px" height="250px" class="solid alignnone size-medium wp-image-3011 rounded-circle image">
+        </a>  
+        <div class="text">
+          <strong>Expression Viewer</strong>
+        </div>
       </div>
-
     </div>
   </div>
-</div>
-
-<script>
-$("#myModal").modal();
-</script>
+</html>
 
 
-
-<style>
-
+<!-- Style css -->
+<style>      
   #welcome_h1 {
     text-align:center;
     margin:auto;
     margin-bottom: 30px;
-    padding-top:200px;
-    color:#fff;
-    font-size:58px;
-    text-shadow: 0px 0px 45px #333;
+    padding-top:10px;
+    color:rgb(101, 63, 40);
+    font-size:48px;
+    /*text-shadow: 0px 0px 40px rgb(115, 109, 107);*/   
+  }
+
+  .select {
+    margin:auto;
+    /*margin:flex;*/
+  }
+
+  .image {
+    background-color:#fff;
+/*    background-image: linear-gradient(#ffffff,#ffffff);*/
+  }
+  .text {
+    text-align:center;
+    font-size:20px;
+  }
+
+  .page_container {
+/*    background-image: linear-gradient(#d9bf9e,#ffffff);*/
+    background-image: linear-gradient(#f9dfbe,#ffffff);
   }
   
-  #button_div {
-    margin: 0;
-    position: absolute;
-    left: 50%;
-    /*margin-top: 30px;*/
-    margin-left: -75px;
-  }
   
-  #start_button {
-    font-size:42px; 
-    line-height:0.9; 
-    width: 150px;
-  }
-
-.cover_img {
-  background-repeat: no-repeat;
-  background-attachment: fixed;  
-  background-size: cover;
-  height: 700px;
-}
-
-.page_container {
-  background-image: <?php echo "url($images_path/welcome_page.jpg)" ?>;
-  background-repeat: no-repeat;
-  background-attachment: fixed;  
-  background-size: cover;
-}
-
-#index_container {
-  max-width:100% !important;
-}
-
-#gdb_footer {
-  margin-top:0px !important;
-}
-
 </style>
-
