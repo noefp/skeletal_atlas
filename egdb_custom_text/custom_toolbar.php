b7cc8e45238fe5570741d3e9b618aa6aad43905f
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-10-18 11:11:13 +0200

    new navigation

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -1,9 +1,11 @@
+<li class="nav-item"><a class="nav-link" href="/skeletal_atlas/egdb_custom_text/tools/expression/expression_input.php">Multiomics Reference Atlas</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/expression_input.php">Experiment Expression Viewer</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/comparator_input.php">Expression comparator</a></li>
+
 <li class="nav-item dropdown">
   <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Tools</a>
   <div class="dropdown-menu">
-    <a class="dropdown-item" href="/skeletal_atlas/egdb_custom_text/tools/expression/expression_input.php">Multiomics Reference Atlas</a>
-    <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_input.php">Experiment Expression Viewer</a>
-    <a class="dropdown-item" href="/easy_gdb/tools/expression/comparator_input.php">Expression comparator</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/search/search_input.php">Gene Annotation Search</a>
     <a class="dropdown-item" href="/easy_gdb/tools/gene_lookup.php">Gene Lookup</a>
   </div>
 </li>
