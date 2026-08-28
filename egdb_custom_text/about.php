6fa58f9c5f4466183f7d3dfbc8626952f89dc46d
Noé Fernández Pozo <noeisneo@gmail.com>
2026-08-28 13:40:33 +0200

    about updated

--- egdb_custom_text/about.php
+++ egdb_custom_text/about.php
@@ -1,10 +1,27 @@
-
-<br>
   <br>
   <h1 style="font-size:32px;" class="text-center">About Skeletal Atlas</h1><br>
   <!-- <h2>About Skeletal Atlas</h2> -->
 
-  <p style="font-size:18px;margin-top:20px;" class="text-justify">
+<div class="card-body">
+  <?php include_once realpath("$custom_text_path/db_citation.php"); ?>
+</div>
+
+<div class="card-body">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      
+      <h4>Code, version, updates and issues:</h4>
+      The SkeletalAtlas is based on <a href="https://github.com/noefp/easy_gdb" target="blank">EasyGDB</a> and its code is available at <a href="https://github.com/noefp/skeletal_atlas" target="blank">GitHub</a>. There, it is possible to known the current version number, last updates and <a href="https://github.com/noefp/skeletal_atlas/issues" target="blank">report any issues</a>.
+      
+    </div>
+  </div>
+</div>
+
+<div class="card-body">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-justify">
+      <h4>The Skeletal Atlas:</h4>
+      
         <p>
          The most comprehensive tool to analyze and extract public results on transcriptomic studies on skeletal tissues. 
         </p>
@@ -20,20 +37,24 @@
         <p>
            Through the features of this atlas the user will be able to performe the following analysis:
         </p>
-<ul>
-  <li>Compare expression data among tissues and specific compartments.</li>
-  <li>Clustering based on known biomarkers: Transcriptomic consultation of well described cell populations (Hypertrophic chondrocytes, osteoclasts, etc)</li>
-  <li>Detection of new biomarkers for skeletal conditions.</li>
-  <li>Co-expression analysis of groups of genes involved in similar processes.</li>
-  <li>Find enriched metabolic pathways and Gene Ontology terms in a selection of a cell population.</li>
-  <li>Representation of the localized expression in a skeletal multi-tissue illustration (from cloud data imaging to in-situ tissue atlas illustration).</li>
-  <li>Gene expression data comparison of multiple datasets for custom bioinformatic studies.</li>
-  <li>Integration and representation of new skeletal expression datasets from open public databases.</li>
-</ul>
+        <ul>
+          <li>Compare expression data among tissues and specific compartments.</li>
+          <li>Clustering based on known biomarkers: Transcriptomic consultation of well described cell populations (Hypertrophic chondrocytes, osteoclasts, etc)</li>
+          <li>Detection of new biomarkers for skeletal conditions.</li>
+          <li>Co-expression analysis of groups of genes involved in similar processes.</li>
+          <li>Find enriched metabolic pathways and Gene Ontology terms in a selection of a cell population.</li>
+          <li>Representation of the localized expression in a skeletal multi-tissue illustration (from cloud data imaging to in-situ tissue atlas illustration).</li>
+          <li>Gene expression data comparison of multiple datasets for custom bioinformatic studies.</li>
+          <li>Integration and representation of new skeletal expression datasets from open public databases.</li>
+        </ul>
         <p>
           Please contact us if you are interested.
         </p>
-  </p>
+    </div>
+  </div>
+</div>
+
+
 <style>
 
 .egdb_person_img {
@@ -43,7 +64,5 @@
 .egdb_person_card:hover {
   transform: scale(1.02);  
 }
-
-
 
 </style>
--- egdb_custom_text/db_citation.php
+++ egdb_custom_text/db_citation.php
@@ -3,13 +3,14 @@
 
     <h4>Please, when using this web site or its data, cite us using the reference:</h4>
 
-    <div class="card bg-light" style="height:100px">
-      <div class= "card-body">
-        Perez-Tejeiro JM, Csukasi F, Fernandez-Pozo N, Duran I (2023).
-        SkeletalAtlas: A gene expression atlas tool for skeletal tissue.<br>
-        <a href="paper_link" target="blank">Journal, 1(1), 111</a><br>
+    <div class="card bg-light">
+      <div class= "card-body text-justify">
+        Fernando Silva†, Jose Miguel Perez-Tejeiro†, Jesus Arcedo, Javier Santos, Rafael Salguero, Ivan Delgado-Sanchez, Sandra Escalante, Alejandro Dominguez, Gretel Nusspaumer, Javier Lopez-Rios, Daniel H. Cohn, Deborah Krakow, Ken To, Intawat Nookaew, Fabiana Csukasi, Sarah Teichmann, Noe Fernandez-Pozo*, Ivan Duran* (2026).
+        <b>The Skeletal Atlas: An Interactive Multi-Omic Resource for Mouse and Human Skeletal Biology and Disease</b><br>
+        <a href="paper_link" target="blank">Journal, 1(1), 111</a>
       </div>
     </div>
 
   </div>
 </div>
+
