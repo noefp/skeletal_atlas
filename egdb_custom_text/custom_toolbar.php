8cea6ae3692415c666b6fa816484aaa8771bc792
Noé Fernández Pozo <noeisneo@gmail.com>
2026-08-28 13:47:58 +0200

    move quick start link

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -52,7 +52,7 @@
 
 
 <!-- <li class="nav-item"><a class="nav-link" href="/easy_gdb/about.php">About</a></li> -->
-<li class="nav-item"><a class="nav-link" href="/tutorial/Skeletal_Atlas_Quick_Start_Tutorial.pdf" target="_blank">Quick Start</a></li>
+<!-- <li class="nav-item"><a class="nav-link" href="/tutorial/Skeletal_Atlas_Quick_Start_Tutorial.pdf" target="_blank">Quick Start</a></li> -->
 
 <style>
   .dropdown-menu{
